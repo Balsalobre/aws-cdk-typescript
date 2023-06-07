b402ee8e2b8e51292e7751533968eaf115279974
balsalobre <carlosocb@gmail.com>
2023-06-08 00:21:31 +0200

    feat: :zap: deploy cdk first starter stack

--- cdk-stater/bin/cdk-stater.ts
+++ cdk-stater/bin/cdk-stater.ts
@@ -4,18 +4,6 @@ import * as cdk from 'aws-cdk-lib';
 import { CdkStaterStack } from '../lib/cdk-stater-stack';
 
 const app = new cdk.App();
-new CdkStaterStack(app, 'CdkStaterStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+// Inicializa un stack de CDK con el nombre CdkStaterStack
+// podemos tener multiples stacks en un proyecto CDK pero con diferentes nombres
+new CdkStaterStack(app, 'CdkFirstStaterStack');
