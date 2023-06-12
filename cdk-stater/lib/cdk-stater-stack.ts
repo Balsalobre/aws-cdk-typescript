796ad3017cb823e0a04585659816c565a28ad465
balsalobre <carlosocb@gmail.com>
2023-06-12 21:10:29 +0200

    feat: :zap: difference between three types of constructs

--- cdk-stater/lib/cdk-stater-stack.ts
+++ cdk-stater/lib/cdk-stater-stack.ts
@@ -1,9 +1,45 @@
-import * as cdk from 'aws-cdk-lib';
+import { Stack, StackProps, Duration } from 'aws-cdk-lib';
+import { Bucket, CfnBucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class CdkStaterStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+// 3. Using a Bucket construct with a custom resource
+class L3Bucket extends Construct {
+  constructor(scope: Construct, id: string, expirationInDays: number) {
+    super(scope, id);
+
+    new Bucket(this, 'L3Bucket', {
+      lifecycleRules: [{
+        expiration: Duration.days(expirationInDays)
+      }]
+    });
+  }
+}
+
+export class CdkStaterStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    // Create an S3 bucket 3 ways to do it
+
+    // 1. Directly
+    new CfnBucket(this, 'MyL1Bucket', {
+      lifecycleConfiguration: {
+        rules: [{
+          expirationInDays: 1,
+          status: 'Enabled'
+        }]
+
+      }
+    });
+
+    // 2. Using a Bucket construct
+    new Bucket(this, 'MyL2Bucket', {
+      lifecycleRules: [{
+        expiration: Duration.days(2)
+      }]
+    });
+
+    // 3. Using a Bucket construct with a custom resource
+    new L3Bucket(this, 'MyL3Bucket', 3);
   }
 }
